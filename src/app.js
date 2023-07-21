6776ba1cbc0486634d82213b97bbc4cd9c93dd6b
harish242 <120125264+harish242@users.noreply.github.com>
2023-07-21 13:40:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
+app.get('/api/get-next-num',(req,res)=>{
+  const{num}=req.body
+  if(num){
+    return res.json({message:num+1,status:'success'})
+  }
+  res.json({status:'failure'})
+})
+
 
 module.exports = app;
